6a270178cafc2fb608051902e61dfbdd22aaed17
hoedhub <hoedmail@gmail.com>
2022-08-17 22:06:53 +0700

    change all

---
+++ server.js
@@ -0,0 +1,19 @@
+const port =
+  process.env.OPENSHIFT_NODEJS_PORT ||
+  process.env.VCAP_APP_PORT ||
+  process.env.PORT ||
+  process.argv[2] ||
+  8765;
+const express = require("express");
+const Gun = require("gun");
+
+const app = express();
+//const port = 8000;
+app.use(Gun.serve);
+app.use(express.static(__dirname));
+
+const server = app.listen(port, () => {
+  console.log("Listening at: http://localhost://" + port+__dirname);
+});
+
+Gun({ web: server });
